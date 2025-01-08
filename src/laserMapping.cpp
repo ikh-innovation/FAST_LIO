8bd672827f46feb29bac6204b991b15432641ef0
k-alev <kos.alevizos@gmail.com>
2025-01-08 18:01:36 +0200

    robot_localization rtk-lio fusion related changes

--- src/laserMapping.cpp
+++ src/laserMapping.cpp
@@ -857,7 +857,7 @@ int main(int argc, char** argv)
     ros::Publisher pubLaserCloudMap = nh.advertise<sensor_msgs::PointCloud2>
             ("/Laser_map", 100000);
     ros::Publisher pubOdomAftMapped = nh.advertise<nav_msgs::Odometry> 
-            ("/Odometry", 100000);
+            ("Odometry", 100000);
     ros::Publisher pubPath          = nh.advertise<nav_msgs::Path> 
             ("/path", 100000);
 //------------------------------------------------------------------------------------------------------

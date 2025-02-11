7a0372cb40ce96c42487220768a73c800c980202
k-alev <kos.alevizos@gmail.com>
2025-02-11 20:28:51 +0200

    heading_relay odometry publisher added

--- aristos/scripts/heading_relay.py
+++ aristos/scripts/heading_relay.py
@@ -6,6 +6,8 @@ from sensor_msgs.msg import Imu
 import math
 from collections import deque
 from threading import Lock
+import tf
+from tf import transformations
 
 class HeadingRelay:
     def __init__(self):
@@ -13,6 +15,7 @@ class HeadingRelay:
         self.odom_topic = rospy.get_param('~odom_topic', 'odom')
         self.imu_topic = rospy.get_param('~imu_topic', 'imu')
         self.repub_imu_topic = rospy.get_param('~repub_imu_topic', 'imu_filtered')
+        self.repub_odom_topic = rospy.get_param('~repub_odom_topic', 'odom_filtered')
         self.distance_threshold = rospy.get_param('~distance_threshold', 1.0)
         self.velocity_threshold = rospy.get_param('~velocity_threshold', 0.5)
         self.window_size = rospy.get_param('~window_size', 10)
@@ -20,6 +23,7 @@ class HeadingRelay:
         
         # State variables
         self.reset()
+        self.tf_listener = tf.TransformListener()
 
         # Locks
         self.lock = Lock()
@@ -30,6 +34,7 @@ class HeadingRelay:
         
         # Publisher
         self.imu_pub = rospy.Publisher(self.repub_imu_topic, Imu, queue_size=10)
+        self.odom_pub = rospy.Publisher(self.repub_odom_topic, Odometry, queue_size=10)
 
     def reset(self):
         self.prev_x = None
@@ -37,6 +42,7 @@ class HeadingRelay:
         self.prev_time = None
         self.distances = deque(maxlen=self.window_size)
         self.current_velocity = 0.0
+        self.odom_msg = Odometry()
 
     def odom_callback(self, msg):
         with self.lock:
@@ -56,7 +62,25 @@ class HeadingRelay:
                     
                     self.distances.append(distance)
                     self.current_velocity = velocity
-            
+
+                    # x_dot = dx/dt
+                    # y_dot = dy/dt
+                    # self.odom_msg.twist.twist.linear.x = x_dot
+                    # self.odom_msg.twist.twist.linear.y = y_dot
+
+                    # Compute twist in the child_frame_id reference frame
+                    try:
+                        (trans, quat) = self.tf_listener.lookupTransform(msg.child_frame_id, msg.header.frame_id, rospy.Time(0))
+                        R = transformations.quaternion_matrix(quat)
+
+                        twist_x = (dx / dt) * R[0][0] - (dy / dt) * R[0][1]
+                        twist_y = (dx / dt) * R[1][0] + (dy / dt) * R[1][1]
+                        self.odom_msg = msg
+                        self.odom_msg.twist.twist.linear.x = twist_x
+                        self.odom_msg.twist.twist.linear.y = twist_y
+                    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                        rospy.logwarn("Transform lookup failed")
+                
             self.prev_x = x
             self.prev_y = y
             self.prev_time = current_time
@@ -70,6 +94,7 @@ class HeadingRelay:
                 # print("current_velocity: {}".format(self.current_velocity))
                 if total_distance > self.distance_threshold and self.current_velocity > self.velocity_threshold and current_time-self.prev_time<self.timeout:
                     self.imu_pub.publish(msg)
+                    self.odom_pub.publish(self.odom_msg)
         
 if __name__ == '__main__':
     try:
